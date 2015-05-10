d965e40be8728846ebb607937ad40e863e774e72
Justin <ArrayPro07@gmail.com>
2015-05-10 17:23:06 -0400

    Added programName explanation

--- src/APITest.java
+++ src/APITest.java
@@ -10,8 +10,12 @@ public class APITest {
         // use.
         LunaServer server = new LunaServer("localhost:415", "testKey");
 
+        // This is optional but useful for determining where requests come from.
+        server.programName = "java_api_example";
+
         // Define where the script is located. If the script is packaged inside
-        // the .jar, use */ as a prefix. The third argument is the "run name", or the name that the database will know the file as.
+        // the .jar, use */ as a prefix. The third argument is the "run name",
+        // or the name that the database will know the file as.
         LunaDBScript script = new LunaDBScript("*/test.luna-scr", "testCategory", "test");
 
         try {
@@ -21,7 +25,7 @@ public class APITest {
             // inside the packaged JAR you must specify the main class object,
             // otherwise set it to null.
             System.out.println("Sent success?: " + script.send(server, APITest.class));
-            
+
             // The script.run() method only takes our server object, and returns
             // an ArrayList of strings (result of each line). This program
             // simply prints them on the screen.
